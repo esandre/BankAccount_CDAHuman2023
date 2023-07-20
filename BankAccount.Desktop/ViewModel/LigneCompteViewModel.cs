c99d8adad53f7248c5a38abe47fc373da1293fa0
Enzo Sandré <atelier@enzosandre.fr>
2023-07-20 15:05:19 +0200

    Données exemple WPF

---
+++ BankAccount.Desktop/ViewModel/LigneCompteViewModel.cs
@@ -0,0 +1,3 @@
+﻿namespace BankAccount.Desktop.ViewModel;
+
+public record LigneCompteViewModel(string Date, string Crédit, string Débit, string SoldeAprèsOpération);
---
+++ BankAccount.Desktop/ViewModel/RelevéCompteViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BankAccount.Desktop.ViewModel;
+
+public class RelevéCompteViewModel : IEnumerable<LigneCompteViewModel>
+{
+    /// <inheritdoc />
+    public IEnumerator<LigneCompteViewModel> GetEnumerator()
+        => new []
+        {
+            new LigneCompteViewModel(DateTime.Now.ToString(), " 3.00 €", "", "3 00€")
+
+        }.Cast<LigneCompteViewModel>().GetEnumerator();
+
+    /// <inheritdoc />
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
